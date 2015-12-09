e0777552424ee9bce9bfca362dce2a34639c2353
David Jiang <dnjiang@stanford.edu>
2015-12-09 12:43:56 -0800

    asldkfn

--- programs/thesaurus.py
+++ programs/thesaurus.py
@@ -9,7 +9,12 @@ def get_synonyms(word, POS):
 		return cache[word]
 	else:
 		url = "http://words.bighugelabs.com/api/2/a3ac0f829c4bc4960c74078b6e4ed9b1/"+word + "/json"
-		data = json.load(urllib2.urlopen(url))
-		synonyms = list(data[POS]["syn"])
-		cache[word] = synonyms
-		return synonyms
+		try:
+			data = json.load(urllib2.urlopen(url))
+			synonyms = list(data[POS]["syn"])
+			cache[word] = synonyms
+			return synonyms
+		except:
+			return ""
+
+get_synonyms("ran","verb")
